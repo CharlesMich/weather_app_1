ee498f8c77372ec43cd1b5d3433676203358bb2f
CharlesMich <charleedongo@hotmail.com>
2025-04-06 15:22:46 -0500

    api call completed

---
+++ weather_app_1/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  weather_app_1
+//
+//  Created by Charles Michael on 4/6/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
