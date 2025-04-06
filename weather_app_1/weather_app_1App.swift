b7bbab448561281db2a91bcb17edead85f5f0452
CharlesMich <charleedongo@hotmail.com>
2025-04-06 14:17:30 -0500

    Initial Commit

---
+++ weather_app_1/weather_app_1App.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_1App.swift
+//  weather_app_1
+//
+//  Created by Charles Michael on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
