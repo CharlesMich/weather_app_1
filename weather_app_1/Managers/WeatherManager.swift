be03a5dbbc144108d03a22efb3927e8bff89301f
CharlesMich <charleedongo@hotmail.com>
2025-04-07 23:27:46 -0500

    completed api call

--- weather_app_1/Managers/WeatherManager.swift
+++ weather_app_1/Managers/WeatherManager.swift
@@ -11,12 +11,11 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/3.0/onecall?lat=\(latitude)&lon=\(longitude)&exclude={part}&appid=\("210df14087fe7cd5874b466fd44a1402")") else { fatalError("Missing URL")}
+        guard let url = URL(string:  "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=210df14087fe7cd5874b466fd44a1402&units=metric") else { fatalError("Missing URL")}
         
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
-        
         let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
         return decodedData
     }
--- weather_app_1/Views/ContentView.swift
+++ weather_app_1/Views/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     var body: some View {
         VStack {
             if let location = locationManager.location {
-                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                        }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
@@ -22,10 +35,11 @@ struct ContentView: View {
                 }
             }
         }
-        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
-        .preferredColorScheme(.dark)
+                .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .preferredColorScheme(.dark)
     }
 }
+
 
 #Preview {
     ContentView()
