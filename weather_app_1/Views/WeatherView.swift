39d58c274e2fc9485a5f5caeb8226cae4a4893df
CharlesMich <charleedongo@hotmail.com>
2025-04-07 23:47:27 -0500

    complete

--- weather_app_1/Views/WeatherView.swift
+++ weather_app_1/Views/WeatherView.swift
@@ -8,34 +8,45 @@
 import SwiftUI
 
 struct WeatherView: View {
+    
     var weather: ResponseBody
+    
     var body: some View {
         ZStack(alignment: .leading){
             VStack {
                 VStack(alignment: .leading, spacing: 5) {
                     Text(weather.name)
-                        .bold().font(.title)
+                        .bold()
+                        .font(.title)
+                    
                     Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
                         .fontWeight(.light)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
+                
                 Spacer()
+                
                 VStack {
                     HStack {
                         VStack (spacing: 20) {
-                            Image(systemName: "sun.max")
+                            Image(systemName: "cloud")
                                 .font(.system(size: 40))
+                            
                             Text(weather.weather[0].main)
                         }
                         .frame(width: 150, alignment: .leading)
+                        
                         Spacer()
+                        
                         Text(weather.main.feelsLike.roundDouble() + "°")
                             .font(.system(size: 80))
                             .fontWeight(.bold)
                             .padding()
                     }
+                    
                     Spacer()
                         .frame(height: 80)
+                    
                     AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")){ image in
                         image.resizable()
                             .aspectRatio(contentMode: .fit)
@@ -43,13 +54,28 @@ struct WeatherView: View {
                     } placeholder: {
                         ProgressView()
                     }
+                    
                     Spacer()
+                }
                 }
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 VStack {
                     Spacer()
                     VStack(alignment: .leading, spacing: 20){
+                        Text("Weather now")
+                            .bold()
+                            .padding(.bottom)
                         
+                        HStack {
+                            WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + ("°")))
+                            Spacer()
+                            WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + ("°")))
+                        }
+                        HStack {
+                            WeatherRow(logo: "wind", name: "wind speed", value: (weather.main.tempMin.roundDouble() + "m/s"))
+                            Spacer()
+                            WeatherRow(logo: "humidity", name: "Humidity", value: "\(weather.main.tempMax.roundDouble())%")
+                        }
                     }
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
@@ -58,9 +84,6 @@ struct WeatherView: View {
                     .background(.white)
                     .cornerRadius(20, corners: [.topLeft, .topRight])
                 }
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
